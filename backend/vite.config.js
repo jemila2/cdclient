61f4bac6c15c43c549b04e7df8b1f0ecd8509121
jemila <abubakarjemiletu08@gmail.com>
2025-08-28 07:51:55 +0100

    first changes

--- backend/vite.config.js
+++ backend/vite.config.js
@@ -8,7 +8,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-   base: '/jemilalaundryb/',
+   base: '/cdclient/',
   server: {
     open: true,
     port: 3001
